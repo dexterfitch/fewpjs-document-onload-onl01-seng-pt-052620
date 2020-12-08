ab3767cf7ddc9df6488985aa52549e923c6547c4
Dexter Fitch <fitch.dexter@gmail.com>
2020-12-08 21:44:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
 });
+
+console.log("This fires before DOMContentLoaded, as soon as index.js loads.")
